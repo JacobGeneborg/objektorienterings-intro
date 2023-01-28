7a424cb912d5e92c0954264ad0018d41a156f0c7
Jacob Geneborg <jacob.geneborg@skola.taby.se>
2023-01-28 12:10:14 +0100

    comit

--- flygplats.py
+++ flygplats.py
@@ -19,6 +19,19 @@ class Airplane():
         print(
             f"{self.model}. Wingspan: {self.wingspan}. Capacity: {self.capacity}")
 
+    def set_wingspan(self, new_wingspan):
+        self.wingspan = new_wingspan
+    
+    def set_capacity(self, new_capacity):
+        self.capacity = new_capacity
+
+    def get_capacity(self):
+        return self.capacity
+        
+
+        
+
+
 
 # ----------Huvudprogram----------
 # Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
@@ -26,3 +39,24 @@ class Airplane():
 a_plane = Airplane("Boeing 747", 64.6, 500)
 a_plane.model = 'Airbus 380'
 a_plane.print_info()
+
+b_plane = Airplane("JAS 39 Gripen", 8.4, 2)
+b_plane.model = "Stritsplan"
+b_plane.print_info()
+
+c_plane = Airplane("art 72", 27, 74)
+c_plane.model = 'Airbus'
+c_plane.print_info()
+
+
+a_plane.set_wingspan(70)
+a_plane.set_capacity(400)
+a_plane.print_info()
+
+b_plane.set_wingspan(10)
+b_plane.set_capacity(1)
+b_plane.print_info()
+
+the_capacity=a_plane.get_capacity()
+print(the_capacity)
+
